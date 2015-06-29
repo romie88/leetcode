d69a1dcbb85991738a229060853c370374a8dcd2
romie <romie88@gmail.com>
2015-06-29 04:05:03 -0400

    Update 229 Majority Element II: add a different implementation of Moore voting

--- 229.Majority.Element.II.h
+++ 229.Majority.Element.II.h
@@ -18,6 +18,13 @@
 class Solution {
 public:
     std::vector< int > majorityElement( const std::vector< int > & nums ) {
+        return majority_element_2( nums );
+    }
+private:
+    /**
+     * Moore voting implementation 1
+     */
+    std::vector< int > majority_element_1( const std::vector< int > & nums ) {
         std::vector< int > result;
         if ( nums.empty() ) return result;
         else if ( nums.size() == 1 ) {
@@ -54,20 +61,68 @@ public:
             }
         }
         
-        c1 = 0;
-        c2 = 0;
-        for ( i = 0; i < nums.size(); ++i ) {
-            if ( nums[ i ] == n1 )
+        verify_result( nums, n1, n2, result );
+        return result;
+    }
+    
+    /**
+     * Moore voting implementation 2
+     */
+    std::vector< int > majority_element_2( const std::vector< int > & nums ) {
+        std::vector< int > result;
+        int n1 = 0, n2 = 0;
+        int c1 = 0, c2 = 0;
+        for ( int i = 0; i < nums.size(); ++i ) {
+            int n3 = nums[ i ];
+            if ( c1 > 0 && c2 > 0 ) {
+                if ( n3 != n1 && n3 != n2 ) {
+                    --c1;
+                    --c2;
+                } else if ( n3 == n1 ) {
+                    ++c1;
+                } else {
+                    ++c2;
+                }
+            } else if ( c1 > 0 ) {
+                if ( n3 == n1 ) {
+                    ++c1;
+                } else {
+                    n2 = n3;
+                    ++c2;
+                }
+            } else if ( c2 > 0 ) {
+                if ( n3 == n2 ) {
+                    ++c2;
+                } else {
+                    n1 = n3;
+                    ++c1;
+                }
+            } else {
+                n1 = n3;
                 ++c1;
-            else if ( nums[ i ] == n2 )
+            }
+        }
+        
+        verify_result( nums, n1, n2, result );
+        return result;
+    }
+    
+    void verify_result( const std::vector< int > & nums,
+                        const int candidate1,
+                        const int candidate2,
+                        std::vector< int > & result ) {
+        int c1 = 0;
+        int c2 = 0;
+        for ( int i = 0; i < nums.size(); ++i ) {
+            if ( nums[ i ] == candidate1 )
+                ++c1;
+            else if ( nums[ i ] == candidate2 )
                 ++c2;
         }
         
         if ( c1 > nums.size() / 3 )
-            result.push_back( n1 );
+            result.push_back( candidate1 );
         if ( c2 > nums.size() / 3 )
-            result.push_back( n2 );
-        
-        return result;
+            result.push_back( candidate2 );
     }
 };
