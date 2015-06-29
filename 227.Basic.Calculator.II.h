e9e10a74c453d3dfba9e497c570c6d93aea7bba3
romie <romie88@gmail.com>
2015-06-28 22:08:51 -0400

    Update 227 Basic Calculator: directly evaluate the infix expression

--- 227.Basic.Calculator.II.h
+++ 227.Basic.Calculator.II.h
@@ -31,10 +31,41 @@
 class Solution {
 public:
     int calculate( const std::string & s ) {
+        return calculate_impl_directly( s );
+    }
+private:
+    int calculate_impl_by_postfix( const std::string & s ) {
         std::string postfix = infix_to_postfix( s );
         return evaluate_postfix( postfix );
     }
-private:
+    
+    void process_operator( std::stack< char > & operators_stack,
+                           std::stack< int >  & operands_stack ) {
+        char op = operators_stack.top();
+        operators_stack.pop();
+        
+        if ( operands_stack.empty() )
+            throw std::invalid_argument( "missing operand" );
+        int op2 = operands_stack.top();
+        operands_stack.pop();
+        
+        if ( operands_stack.empty() )
+            throw std::invalid_argument( "missing operand" );
+        int op1 = operands_stack.top();
+        operands_stack.pop();
+        
+        int result = 0;
+        switch ( op ) {
+            case '+': result = op1 + op2; break;
+            case '-': result = op1 - op2; break;
+            case '*': result = op1 * op2; break;
+            case '/': result = op1 / op2; break;
+            default:
+                throw std::invalid_argument( "unknown operator" );
+        }
+        operands_stack.push( result );
+    }
+    
     int op_precedence( const char op ) {
         switch ( op ) {
             case '+':
@@ -140,5 +171,54 @@ private:
         }
         if ( stk.size() > 1 ) throw std::invalid_argument( "operands left" );
         return stk.top();
+    }
+    
+    /**
+     * Implementation directly evaluate the infix expression
+     */
+    int calculate_impl_directly( const std::string & s ) {
+        std::stack< char > operators_stack;
+        std::stack< int > operands_stack;
+        for ( int i = 0; i < s.length(); ++i ) {
+            if ( std::isdigit( s[ i ] ) ) {
+                int num = 0;
+                while ( std::isdigit( s[ i ] ) ) {
+                    num = num * 10 + s[ i ] - '0';
+                    ++i;
+                }
+                --i;
+                operands_stack.push( num );
+            } else if ( s[ i ] == '+'
+                     || s[ i ] == '-'
+                     || s[ i ] == '*'
+                     || s[ i ] == '/'
+                     || s[ i ] == '(' ) {
+                while ( ! operators_stack.empty()
+                     && op_precedence( operators_stack.top() )
+                     >= op_precedence( s[ i ] ) ) {
+                    process_operator( operators_stack, operands_stack );
+                }
+                operators_stack.push( s[ i ] );
+            } else if ( s[ i ] == ')' ) {
+                while ( ! operators_stack.empty()
+                     && operators_stack.top() != '(' ) {
+                    process_operator( operators_stack, operands_stack );
+                }
+                if ( operators_stack.top() == '(' ) {
+                    operators_stack.pop();
+                } else {
+                    throw std::invalid_argument( "unmatched parenthese" );
+                }
+            } else if ( ! std::isspace( s[ i ] ) ) {
+                throw std::invalid_argument( "unknown character" );
+            }
+        }
+        while ( ! operators_stack.empty() ) {
+            process_operator( operators_stack, operands_stack );
+        }
+        if ( operands_stack.size() > 1 )
+            throw std::invalid_argument( "invalid expression" );
+        else
+            return operands_stack.top();
     }
 };
