3de54a40c30125b75d30c7c7bb94b01a492a2d09
romie <romie88@gmail.com>
2015-07-10 17:34:49 -0400

    Add 19 Remove Nth Node From End of List

--- 19.Remove.Nth.Node.From.End.of.List.h
+++ 19.Remove.Nth.Node.From.End.of.List.h
@@ -1,19 +1,23 @@
-//19 Remove Nth Node From End of List [ Easy ]
-//
-//Given a linked list, remove the nth node from the end of list and return
-//its head.
-//
-//For example,
-//
-//   Given linked list: 1->2->3->4->5, and n = 2.
-//
-//   After removing the second node from the end, the linked list becomes 1->2->3->5.
-//
-//Note:
-//Given n will always be valid.
-//Try to do this in one pass.
-//
-//Tags: Linked List, Two Pointers
+/**
+ * Algorithms 19 Remove Nth Node From End of List                          Easy
+ * 
+ * Given a linked list, remove the n-th node from the end of list and return
+ * its head.
+ * 
+ * For example,
+ * 
+ *    Given linked list: 1->2->3->4->5, and n = 2.
+ * 
+ *    After removing the second node from the end, the linked list becomes
+ *    1->2->3->5.
+ * 
+ * Note:
+ * 
+ * Given n will always be valid.
+ * Try to do this in one pass.
+ * 
+ * Tags: Linked List, Two Pointers
+ */
 
 #include <stdexcept>
 
@@ -29,37 +33,33 @@ class Solution {
 public:
     ListNode * removeNthFromEnd( ListNode * head, int n ) {
         
-        if ( n <= 0 ) throw std::invalid_argument( "n must be positive" );
+        if ( n <= 0 ) throw std::out_of_range( "n is not positive" );
         
-        //need to find the node ahead of the Nth node from the end
-        ListNode * p1 = head;
-        ++n;
-        while ( p1 && n ) {
+        ListNode dummy_head( 0 );
+        dummy_head.next = head;
+        
+        //move p1 forward n steps first
+        ListNode * p1 = &dummy_head;
+        while ( p1->next && n ) {
             p1 = p1->next;
             --n;
         }
-
-        if ( p1 == nullptr && n > 1 )
-            throw std::invalid_argument( "k is longer than the list length" );
-
-        //remove head
-        if ( p1 == nullptr && n == 1 ) {
-            ListNode * tmp = head;
-            head = head->next;
-            delete tmp;
-            return head;
-        }
+        if ( n > 0 )
+            throw std::out_of_range( "n is larger than the number of nodes" );
         
-        ListNode * p2 = head;
-        while ( p1 ) {
-            p1 = p1->next;
+        ListNode * p2 = &dummy_head;
+        while ( p1->next ) {
             p2 = p2->next;
+            p1 = p1->next;
         }
-
-        ListNode * tmp = p2->next;
-        p2->next = p2->next ? p2->next->next : nullptr;
         
-        return head;
+        ListNode * p_deleted = p2->next;
+        if ( p_deleted ) {
+            p2->next = p_deleted->next;
+            delete p_deleted;
+        }
+        
+        return dummy_head.next;
     }
 };
 
