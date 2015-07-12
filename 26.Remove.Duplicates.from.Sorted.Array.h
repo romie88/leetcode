5372c01f5a84fd2b2b8daa83bb5be4624ca18f45
romie <romie88@gmail.com>
2015-07-12 01:31:25 -0400

    Update 26 Remove Duplicates from Sorted Array

--- 26.Remove.Duplicates.from.Sorted.Array.h
+++ 26.Remove.Duplicates.from.Sorted.Array.h
@@ -29,3 +29,19 @@ public:
         return j;
     }
 };
+//Another similar implementation
+#if 0
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if ( nums.empty() ) return 0;
+        int j = 0;
+        for ( int i = 1; i < nums.size(); ++i ) {
+            if ( nums[ j ] != nums[ i ] ) {
+                nums[ ++j ] = nums[ i ];
+            }
+        }
+        return j + 1;
+    }
+};
+#endif
