af8dccad365e6aff5027c95e0b00321a0af249c8
romie <romie88@gmail.com>
2015-07-16 01:34:48 -0400

    Add 237 Delete Node in a Linked List

---
+++ 237.Delete.Node.in.a.Linked.List.h
@@ -0,0 +1,32 @@
+/**
+ * Algorithms 237 Delete Node in a Linked List                             Easy
+ * 
+ * Write a function to delete a node (except the tail) in a singly linked list,
+ * given only access to that node.
+ * 
+ * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third
+ * node with value 3, the linked list should become 1 -> 2 -> 4 after calling
+ * your function.
+ * 
+ * Tags: Linked List
+ * Similar Problems: (E) Remove Linked List Elements
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode( ListNode * node ) {
+        if ( node == nullptr ) return;
+        ListNode * next = node->next;
+        if ( next == nullptr ) return;
+        *node = *next;
+        delete next; 
+    }
+};
