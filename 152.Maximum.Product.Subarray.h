b5b191dcb0397c48ce57bab6bc433b1a20ce7c89
romie <romie88@gmail.com>
2015-07-21 02:12:29 -0400

    Update 152 Maximum Product Subarray

--- 152.Maximum.Product.Subarray.h
+++ 152.Maximum.Product.Subarray.h
@@ -8,6 +8,7 @@
  * the contiguous subarray [2,3] has the largest product = 6.
  *
  * Tags: Array, Dynamic Programming
+ * Similar Problems: (M) Maximum Subarray, (M) Product of Array Except Self
  */
 
 #include <vector>
@@ -35,3 +36,29 @@ public:
         return max_prod;
     }
 };
+
+/**
+ * Solution
+ * 
+ * Besides keeping track of the largest product, we also need to keep track of
+ * the smallest product. Why? The smallest product, which is the largest in the
+ * negative sense could become the maximum when being multiplied by a negative
+ * number.
+ * 
+ * Let us denote that:
+ * 
+ * f(k) = Largest product subarray, from index 0 up to k.
+ * 
+ * Similarly,
+ * 
+ * g(k) = Smallest product subarray, from index 0 up to k.
+ * 
+ * Then,
+ * 
+ * f(k) = max( f(k-1) * A[k], A[k], g(k-1) * A[k] )
+ * g(k) = min( g(k-1) * A[k], A[k], f(k-1) * A[k] )
+ * 
+ * There we have a dynamic programming formula. Using two arrays of size n, we
+ * could deduce the final answer as f(n-1). Since we only need to access its
+ * previous elements at each step, two variables are sufficient.
+ */
