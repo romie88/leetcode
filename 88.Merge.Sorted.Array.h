4fdcd2624bc9e3bf845e17777039cc2dc162b027
romie <romie88@gmail.com>
2015-08-11 05:52:33 -0400

    Update 88 Merge Sorted Array - add Similar Problems

--- 88.Merge.Sorted.Array.h
+++ 88.Merge.Sorted.Array.h
@@ -1,15 +1,16 @@
 /**
  * 88 Merge Sorted Array                                                   Easy
- * 
+ *
  * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
  * one sorted array.
- * 
+ *
  * Note:
  * You may assume that nums1 has enough space (size that is greater or equal to
  * m + n) to hold additional elements from nums2. The number of elements
  * initialized in nums1 and nums2 are m and n respectively.
- * 
+ *
  * Tags: Array, Two Pointers
+ * Similar Problems: (E) Merge Two Sorted Lists
  */
 
 #include <vector>
