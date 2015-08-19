46b96c39f7a0e589eed595f2ad1ed148ca43513f
romie <romie88@gmail.com>
2015-08-19 02:50:37 -0400

    update 95 Unique Binary Search Trees II - add Similar Problems

--- 95.Unique.Binary.Search.Trees.II.h
+++ 95.Unique.Binary.Search.Trees.II.h
@@ -1,25 +1,25 @@
 /**
  * Algorithms 95 Unique Binary Search Trees II                           Medium
- * 
+ *
  * Given n, how many structurally unique BST's (binary search trees) that store
  * values 1...n?
- * 
+ *
  * For example,
  * Given n = 3, there are a total of 5 unique BST's.
- * 
+ *
  *    1         3     3      2      1
  *     \       /     /      / \      \
  *      3     2     1      1   3      2
  *     /     /       \                 \
  *    2     1         2                 3
- * 
+ *
  * confused what "{1,#,2,3}" means? > read more on how binary tree is
  * serialized on OJ.
- * 
+ *
  * OJ's Binary Tree Serialization:
  * The serialization of a binary tree follows a level order traversal, where
  * '#' signifies a path terminator where no node exists below.
- * 
+ *
  * Here's an example:
  *    1
  *   / \
@@ -29,8 +29,10 @@
  *     \
  *     5
  * The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
- * 
+ *
  * Tags: Tree, Dynamic Programming
+ * Similar Problems: (M) Unique Binary Search Trees,
+ *                   (M) Different Ways to Add Parentheses
  */
 
 #include <vector>
@@ -56,7 +58,7 @@ private:
             trees.push_back( nullptr );
             return trees;
         }
-        
+
         for ( int i = start; i <= end; ++i ) {
             //change std::vector< TreeNode * > to const std::vector< TreeNode * > &
             //greatly enhanced the running time
