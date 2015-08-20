a4caaad86ab33c0a45f85102c86ca6d753ee15eb
romie <romie88@gmail.com>
2015-08-19 23:33:42 -0400

    Update 98 Validate Binary Search Tree - add 'Similar Problems'

--- 98.Validate.Binary.Search.Tree.h
+++ 98.Validate.Binary.Search.Tree.h
@@ -1,23 +1,23 @@
 /**
  * Algorithms 98 Validate Binary Search Tree                             Medium
- * 
+ *
  * Given a binary tree, determine if it is a valid binary search tree (BST).
- * 
+ *
  * Assume a BST is defined as follows:
- * 
+ *
  * * The left subtree of a node contains only nodes with keys less than the
  *   node's key.
  * * The right subtree of a node contains only nodes with keys greater than the
  *   node's key.
  * * Both the left and right subtrees must also be binary search trees.
- * 
+ *
  * confused what "{1,#,2,3}" means? > read more on how binary tree is
  * serialized on OJ.
- * 
+ *
  * OJ's Binary Tree Serialization:
  * The serialization of a binary tree follows a level order traversal, where
  * '#' signifies a path terminator where no node exists below.
- * 
+ *
  * Here's an example:
  *    1
  *   / \
@@ -27,8 +27,9 @@
  *     \
  *     5
  * The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
- * 
+ *
  * Tags: Tree, Depth-first Search
+ * Similar Problems: (M) Binary Tree Inorder Traversal
  */
 
 /**
@@ -70,7 +71,7 @@ private:
             && is_subtree_greater_than( root->left, val )
             && is_subtree_greater_than( root->right, val );
     }
-    
+
     /**
      * O( n ) runtime, O( n ) stack space - Top-down recursion
      * More general than using std::numeric_limits< int >::min() and max()
@@ -89,7 +90,7 @@ private:
             && is_valid_BST_td_impl( root->left, has_lower_bound, true, lower_bound, root->val )
             && is_valid_BST_td_impl( root->right, true, has_upper_bound, root->val, upper_bound );
     }
-    
+
     /**
      * O( n ) runtime, O( n ) stack space - In-order traversal
      */
