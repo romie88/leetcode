717dc259fb21c4bb904cb2fdf40e7e3367f2bd16
romie <romie88@gmail.com>
2015-08-24 23:47:26 -0400

    Update 145 Binary Tree Postorder Traversal - update comments

--- 145.Binary.Tree.Postorder.Traversal.h
+++ 145.Binary.Tree.Postorder.Traversal.h
@@ -1,19 +1,22 @@
-//145 Binary Tree Postorder Traversal [ Hard ]
-//
-//Given a binary tree, return the postorder traversal of its nodes' values.
-//
-//For example:
-//Given binary tree {1,#,2,3},
-//   1
-//    \
-//     2
-//    /
-//   3
-//return [3,2,1].
-//
-//Note: Recursive solution is trivial, could you do it iteratively?
-
-//Tags: Tree, Stack
+/**
+ * Algorithms 145 Binary Tree Postorder Traversal                          Hard
+ *
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ *
+ * For example:
+ * Given binary tree {1,#,2,3},
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ * return [3,2,1].
+ *
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ *
+ * Tags: Tree, Stack
+ * Similar Problems: (M) Binary Tree Inorder Traversal
+ */
 
 #include <vector>
 #include <stack>
@@ -33,42 +36,46 @@ public:
 
     std::vector< int > postorderTraversal( TreeNode * root ) {
         std::vector< int > v;
-        postorder_traversal_recursive( root, v );
+        postorder_traversal_iterative_by_one_stack_and_prev( root, v );
         return v;
     }
 
 private:
-    
-    //straightforward recursive implementation
+
+    /**
+     * O( n ) runtime, O( n ) space - straightforward recursive implementation
+     */
     void postorder_traversal_recursive(
             TreeNode * p,
             std::vector< int > & v ) {
-        
+
         if ( ! p ) return;
-        
+
         postorder_traversal_recursive( p->left,  v );
         postorder_traversal_recursive( p->right, v );
         v.push_back( p->val );
     }
-    
-    //iterative implementation using two stacks explicitly
-    //http://www.geeksforgeeks.org/iterative-postorder-traversal/
-    //The idea is that let's observe the output of a postorder traversal
-    //for example
-    //        1
-    //       / \
-    //      2   3
-    //     / \ / \
-    //    4  5 6  7
-    //4 5 2 6 7 3 1
-    //look at the reverse order of it
-    //1 3 7 6 2 5 4
-    //this looks very similar to the preorder traversal output
-    //except it visits right subtree ahead of left subtree
+
+    /**
+     * iterative implementation using two stacks explicitly
+     * http://www.geeksforgeeks.org/iterative-postorder-traversal/
+     * The idea is that let's observe the output of a postorder traversal
+     * for example
+     *        1
+     *       / \
+     *      2   3
+     *     / \ / \
+     *    4  5 6  7
+     * 4 5 2 6 7 3 1
+     * look at the reverse order of it
+     * 1 3 7 6 2 5 4
+     * this looks very similar to the preorder traversal output
+     * except it visits right subtree ahead of left subtree
+     */
     void postorder_traversal_two_stacks(
             TreeNode * p,
             std::vector< int > & v ) {
-        
+
         std::stack< TreeNode * > s1;
         std::stack< TreeNode * > s2;//used for the VRL preorder traversal
         while ( p || ! s2.empty() ) {
@@ -89,12 +96,15 @@ private:
             s1.pop();
         }
     }
-    
-    //iterative implementation using a stack explicitly
-    void postorder_traversal_one_stack(
+
+    /**
+     * O( n ) runtime, O( n ) space - my version of iterative implementation
+     * using a stack and predecessor pointer
+     */
+    void postorder_traversal_iterative_by_one_stack_and_prev(
             TreeNode * p,
             std::vector< int > & v ) {
-        
+
         std::stack< TreeNode * > s;
         TreeNode * prev = nullptr;
         while ( p || ! s.empty() ) {
@@ -103,12 +113,12 @@ private:
                 p = p->left;
             } else {
                 p = s.top();
-                //the key trick here is when to pop the pointer
-                //to the node of the stack
-                //if the right child is nullptr or it has previously
-                //been visited then it is right to visit the node
-                //pointed by p
-                //otherwise proceed to the right subtree
+                /**
+                 * The key question here is when to pop the pointer off the
+                 * stack. if the right child is nullptr or it matches the
+                 * previously visited pointer then it should be popped off
+                 * the stack. Otherwise just proceed to the right subtree.
+                 */
                 if ( ! p->right || prev == p->right ) {
                     s.pop();
                     v.push_back( p->val );
@@ -121,12 +131,16 @@ private:
         }
     }
 
-    //iterative implementation using Morris threaded binary tree
-    //http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
-    void postorder_traversal_threaded(
-            TreeNode * p,
+    /**
+     * O( n ) runtime, O( n ) space - the iterative traversal by Morris
+     * threaded tree method.
+     *
+     * http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
+     */
+    void postorder_traversal_iterative_by_morris_threaded_tree(
+            TreeNode           * p,
             std::vector< int > & v ) {
-        
+
         TreeNode dump( 0 );
         dump.left = p;
         p = &dump;
@@ -137,7 +151,7 @@ private:
                 TreeNode * tmp = p->left;
                 while ( tmp->right && tmp->right != p )
                     tmp = tmp->right;
-                
+
                 if ( ! tmp->right ) {
                     tmp->right = p;
                     p = p->left;
@@ -155,13 +169,13 @@ private:
                     reverse( tmp, p->left );
                     //note this must happen after the reverse output
                     tmp->right = nullptr;
-                    
+
                     p = p->right;
                 }
             }
         }
     }
-    
+
     void reverse( TreeNode * p1, TreeNode * p2 ) {
 
         if ( p1 == p2 ) return;
