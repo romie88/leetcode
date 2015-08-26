15e697eebce7050cf25219a21b246a8d2a4569ba
romie <romie88@gmail.com>
2015-08-26 00:56:00 -0400

    Update 106 Construct Binary Tree from Inorder and Postorder Traversal

--- 106.Construct.Binary.Tree.from.Inorder.and.Postorder.Traversal.h
+++ 106.Construct.Binary.Tree.from.Inorder.and.Postorder.Traversal.h
@@ -1,12 +1,14 @@
-//106 Construct Binary Tree from Inorder and Postorder Traversal [ Medium ]
-//2012-09-30 Medium
-//
-//Given inorder and postorder traversal of a tree, construct the binary tree.
-//
-//Note:
-//You may assume that duplicates do not exist in the tree.
-
-//Tags: Tree, Array, Depth-first Search
+/**
+ * Algorithms 106 Construct Binary Tree from Inorder and Postorder Traversal   Medium
+ *
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ *
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ *
+ * Tags: Tree, Array, Depth-first Search
+ * Similar Problems: (M) Construct Binary Tree from Preorder and Inorder Traversal
+ */
 
 #include <vector>
 #include <stdexcept>
@@ -22,9 +24,9 @@
  */
 class Solution {
 public:
-    TreeNode * buildTree( const std::vector< int > & inorder, 
+    TreeNode * buildTree( const std::vector< int > & inorder,
                           const std::vector< int > & postorder ) {
-        
+
         if ( inorder.size() != postorder.size() )
             throw std::out_of_range( "inorder and postorder have different size!" );
 
@@ -34,7 +36,7 @@ public:
 private:
     TreeNode * build_tree_impl( const std::vector< int > & inorder,   int s1, int e1,
                                 const std::vector< int > & postorder, int s2, int e2 ) {
-        
+
         if ( s1 >= e1 ) return nullptr;
 
         TreeNode * root = new TreeNode( postorder[ e2 - 1 ] );
@@ -42,12 +44,12 @@ private:
         for ( i = s1; i < e1; ++i )
             if ( inorder[ i ] == postorder[ e2 - 1 ] )
                 break;
-        
+
         root->left  = build_tree_impl( inorder,   s1, i,
                                        postorder, s2, s2 + i - s1 );
         root->right = build_tree_impl( inorder,   i + 1, e1,
                                        postorder, e2 - ( e1 - i ), e2 - 1 );
-        
+
         return root;
     }
 };
